1fd15064ec46e4bdce1bcde4527eb2268ab5e35b
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 15:15:06 -0600

    Update comunion.js

--- js/cantos/comunion.js
+++ js/cantos/comunion.js
@@ -8,28 +8,35 @@ window.CANTOS_COMUNION = [
     notas: "Canto de comunion",
     letra:`
 [coro]
-El Señor nos invita ya
-con amor su cuerpo a comulgar
-ya no es vino
-ya no es pan
-Cristo mismo se nos da
-alelu, alelu, alelu uuya
+E[D]l señ[Bm]or n[Em]os invita y[A7]a
+c[D]on am[Bm]or s[Em]u cuerpo a com[A7]ulgar,
+Y[D]a no es v[Bm]ino, y[Em]a no es p[A7]an,
+Cr[D]isto m[Bm]ismo s[Em]e nos d[A7]a.
+[D]Alelu, [Bm]alelu, [Em]Alelu, [A7]aleluy[D]a.[Bm]-[Em]-[A7]
 
 [verso 1]
-acercarce a comulgar
-es acercarce al Señor
-y de su cena pascual
-tener participacion
+[D]Acercars[Bm]e a c[Em]omulg[A7]ar
+[D]Es acerc[Bm]arse [Em]al señ[A7]or,
+[D]Y de s[Bm]u c[Em]ena pascu[A7]al
+T[D]ener p[Bm]artic[Em]ipación.[A7]
 
-[verso 2]
-con todo el pueblo de Dios
-una fiesta celebrar
-por que Cristo en comunion
-ha venido a nuestro altar
+[Verso 2]
+C[D]on t[Bm]odo el p[Em]ueblo de D[A7]ios
+[D]Una f[Bm]iesta c[Em]elebr[A7]ar,
+P[D]orque Cr[Bm]isto [Em]en comun[A7]ión
+H[D]a v[Bm]enido a n[Em]uestro alt[A7]ar.
 
-[Verso 3]
+[verso 3]
+H[D]oy l[Bm]a fam[Em]ilia de D[A7]ios,
+Qu[D]e en la m[Bm]esa p[Em]arte el p[A7]an
+C[D]ome el c[Bm]uerpo d[Em]el señ[A7]or,
+[D]Y c[Bm]onvive [Em]en la unid[A7]ad.
 
-
+[verso 4]
+[D]Esta s[Bm]anta c[Em]omuni[A7]ón
+[D]Es c[Bm]omida fr[Em]atern[A7]al,
+[D]Es t[Bm]estimoni[Em]o de am[A7]or
+[D]Es b[Bm]anquete f[Em]amili[A7]ar.
 
 `
   }
