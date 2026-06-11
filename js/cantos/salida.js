06160508c1da1caa838e2a2611c2cadda7ae9d23
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 14:55:14 -0600

    Update salida.js

---
+++ js/cantos/salida.js
@@ -0,0 +1,21 @@
+window.CANTOS_SALIDA = [
+  {
+    titulo: "Quiero darte las gracias",
+    categoria: "Salida",
+    tiempo: "ordinario",
+    tono: "G",
+    autor: "Kerigma",
+    notas: "Canto de salida",
+    letra:`
+[coro]
+Qu[G]iero darte las gr[C]acias, mi Señ[G]or,
+Por el don que me d[C]as cada mañ[D]ana,
+
+Por los árb[C]oles, los p[D]ájaros y el s[G]ol, [Em]
+Por la ll[C]uvia que az[D]ota en mi vent[G]ana. (2)
+
+Gr[C]aci[G]as, muchas gr[C]ac[G]ias, m[C]i S[D]eñ[G]or.
+
+`
+  }
+];
