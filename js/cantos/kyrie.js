986d353783c55968f1178075464166c922ba7ba3
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 16:31:15 -0600

    Update and rename Kyrie.js to kyrie.js

---
+++ js/cantos/kyrie.js
@@ -0,0 +1,29 @@
+window.CANTOS_KYRIE = [
+  {
+    titulo: "Kyrie Am",
+    categoria: "Kyrie",
+    tiempo: "ordinario",
+    tono: "Am",
+    autor: "Kerigma",
+    notas: "Canto de Kyrie",
+    letra:`
+[coro]
+Señor ten piedad de nosotros
+Señor ten piedad
+Señor ten piedad de nosotros
+Señor ten piedad
+
+[verso]
+Cristo ten piedad de nosotros
+Cristo ten piedad
+Cristo ten piedad de nosotros
+
+[coro]
+Señor ten piedad de nosotros
+Señor ten piedad
+Señor ten piedad de nosotros
+Señor ten piedad
+
+`
+  }
+];
