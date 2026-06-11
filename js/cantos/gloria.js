ac569c3dc1c5f9a836ae5a17d1c0b815c6fa51fb
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-10 20:02:50 -0600

    Actualizar gloria.js

---
+++ js/cantos/gloria.js
@@ -0,0 +1,13 @@
+window.CANTOS_GLORIA = [
+  {
+    titulo: "Gloria Mejia",
+    categoria: "Gloria",
+    tiempo: "ordinario",
+    tono: "Em",
+    autor: "Kerigma",
+    notas: "Canto del gloria",
+    letra:`
+Gloria, Gloria
+`
+  }
+];
