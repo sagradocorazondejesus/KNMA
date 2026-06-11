91aa8bdc603ded645a1a33fd835909b731c45322
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 16:34:53 -0600

    Update aleluya.js

---
+++ js/cantos/aleluya.js
@@ -0,0 +1,24 @@
+window.CANTOS_ALELUYA = [
+  {
+    titulo: "Aleluya miserere",
+    categoria: "aleluya",
+    tiempo: "ordinario",
+    tono: "G",
+    autor: "Kerigma",
+    notas: "Canto de aleluya",
+    letra:`
+[verso]
+Aleluya, aleluya,
+aleluya, aleluya. (2)
+
+[coro]
+aaaaaa, aaaaaa, aaaaaaa, aaaaaaaa.
+
+
+[verso]
+Aleluya, aleluya,
+aleluya, aleluya.
+
+`
+  }
+];
