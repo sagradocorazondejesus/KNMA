bda22c137e5ca2b3b551326737e737b620d1429c
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-10 22:32:15 -0600

    Update gloria.js

--- js/cantos/gloria.js
+++ js/cantos/gloria.js
@@ -7,14 +7,25 @@ window.CANTOS_GLORIA = [
     autor: "Kerigma",
     notas: "Canto del gloria",
     letra:`
-Gloria, Gloria a Dios en el cielo
-y en la tierra a los hmbres paz
-Te alabamos y te vendecimos
-te adoramos y glorificamos
-y nosotros hoy te damos gracias
-por tu grande y eterna gloria
-Gloria, Gloria a Dios en el cielo
-y en la tierra a los hombres paz
+[coro]
+Gl[Em]oria, Gloria
+[B7]a Dios en el c[Em]ielo
+y [G]en la t[C]ierra
+[B7]a los hmbres p[Em]az (2)
+
+[Vverso 1]
+Te alabamos y
+te vendecimos
+te adoramos
+y glorificamos
+y nosotros hoy
+te damos gracias
+por tu grande
+y eterna gloria
+Gloria, Gloria
+a Dios en el cielo
+y en la tierra
+a los hombres paz
 Señor Dios nuetro
 `
   }
