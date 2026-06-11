3259b263022d723f4dd7ce4e67988cb890cca873
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 16:48:06 -0600

    Update ofertorio.js

--- js/cantos/ofertorio.js
+++ js/cantos/ofertorio.js
@@ -1,26 +1,32 @@
 window.CANTOS_OFERTORIO = [
   {
     titulo: "Hoy Señor",
-    categoria: "cordero",
+    categoria: "ofertorio",
     tiempo: "ordinario",
-    tono: "Em",
+    tono: "C",
     autor: "Kerigma",
-    notas: "Canto de cordero",
+    notas: "Canto de ofertorio",
     letra:`
-    
-Cordero de Dios que quitas
-el pecado del mundo
-ten piedad de nosotros
-ten piedad de nosotros (2)
 
-Cordero de Dios que quitas
-el pecado del mundo
-danos la paz
-danos la paz
-danos, danos,
-danos la paz,
-danos, danos,
-danos la paz.
+[verso]
+Hoy Señor
+venimos a ofrecerte
+este humilde pan
+que es fruto del trabajo
+de la humanidad
+y es signo de fraternidad
+de fe esperanza y amistad
+
+[coro]
+Tomalo aceptalo
+con amor transformalo
+en el cuerpo de jesus
+bendito seas señor
+
+Tomalo aceptalo
+con amor transformalo
+en la sangre de Jesus
+bendito seas Señor
 
 `
   }
