b24fca8349e100370d3197531e4f0d5946ab068a
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 16:42:26 -0600

    Update cordero.js

---
+++ js/cantos/cordero.js
@@ -0,0 +1,27 @@
+window.CANTOS_CORDERO = [
+  {
+    titulo: "",
+    categoria: "cordero",
+    tiempo: "ordinario",
+    tono: "Em",
+    autor: "Kerigma",
+    notas: "Canto de cordero",
+    letra:`
+    
+Cordero de Dios que quitas
+el pecado del mundo
+ten piedad de nosotros
+ten piedad de nosotros (2)
+
+Cordero de Dios que quitas
+el pecado del mundo
+danos la paz
+danos la paz
+danos, danos,
+danos la paz,
+danos, danos,
+danos la paz.
+
+`
+  }
+];
