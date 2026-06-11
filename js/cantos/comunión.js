a644e16f5201dc43aa6c112a52b7c7564f46a550
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 12:41:45 -0600

    Update comunión.js

---
+++ js/cantos/comunión.js
@@ -0,0 +1,15 @@
+window.CANTOS_OTROS = [
+  {
+    titulo: "Espíritu Santo ven, ven",
+    categoria: "comunion",
+    tiempo: "Ordinario",
+    tono: "C",
+    autor: "Kerigma",
+    notas: "Canto del Espiritu Santo",
+    letra:`
+[coro]
+
+
+`
+  }
+];
