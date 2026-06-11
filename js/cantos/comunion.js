dc89dc56bcf0a6ecc824305501169f4e0e573a48
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 13:05:49 -0600

    Replace CANTOS_OTROS with CANTOS_COMUNION

---
+++ js/cantos/comunion.js
@@ -0,0 +1,36 @@
+window.CANTOS_COMUNION = [
+  {
+    titulo: "El Señor nos invita ya",
+    categoria: "comunion",
+    tiempo: "Ordinario",
+    tono: "C",
+    autor: "Kerigma",
+    notas: "Canto de comunion",
+    letra:`
+[coro]
+El Señor nos invita ya
+con amor su cuerpo a comulgar
+ya no es vino
+ya no es pan
+Cristo mismo se nos da
+alelu, alelu, alelu uuya
+
+[verso 1]
+acercarce a comulgar
+es acercarce al Señor
+y de su cena pascual
+tener participacion
+
+[verso 2]
+con todo el pueblo de Dios
+una fiesta celebrar
+por que Cristo en comunion
+ha venido a nuestro altar
+
+[Verso 3]
+
+
+
+`
+  }
+];
