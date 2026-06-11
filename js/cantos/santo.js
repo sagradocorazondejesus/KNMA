dd38d64aa46f01793e7815e3df861d2a67fe95a4
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 16:53:13 -0600

    Update santo.js

---
+++ js/cantos/santo.js
@@ -0,0 +1,33 @@
+window.CANTOS_SANTO = [
+  {
+    titulo: "Santo Papal",
+    categoria: "santo",
+    tiempo: "ordinario",
+    tono: "C#m",
+    autor: "Kerigma",
+    notas: "Canto de Santo",
+    letra:`
+
+Santo, santo, santo
+es el Señor Dios del universo
+llenos están el cielo y la tierra 
+de tu gloria hosanna 
+
+Hosanna, hosanna, hosanna 
+en el cielo 
+hosanna, hosanna, hosanna 
+en el cielo. 
+
+Bendito el que viene 
+en nombre del Señor 
+hosanna en el cielo 
+hosanna. 
+
+Hosanna, hosanna, hosanna 
+en el cielo 
+hosanna, hosanna, hosanna 
+en el cielo. 
+
+`
+  }
+];
