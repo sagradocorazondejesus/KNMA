b19c1461458c988d19b1a4dccaf5d6e9309455e4
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-11 10:39:38 -0600

    Update otros.js

---
+++ js/cantos/otros.js
@@ -0,0 +1,66 @@
+window.CANTOS_OTROS = [
+  {
+    titulo: "Espíritu Santo ven, ven",
+    categoria: "otros",
+    tiempo: "Ordinario",
+    tono: "C",
+    autor: "Kerigma",
+    notas: "Canto del Espiritu Santo",
+    letra:`
+[coro]
+E[D]spíritu Santo ven, ven;
+Espíritu Santo v[A]en, ven
+Esp[D]íritu Santo ven, ven
+En el n[G]ombre d[A]el Señ[D]or
+
+[verso 1]
+[D7]Acomp[G]añame, il[D]uminame
+t[A]oda mi v[D]ida
+A[D7]comp[G]añame ilum[D]iname
+Esp[A]íritu Santo v[D]en, ven
+
+[veros 2]
+P[D7]urif[G]icame, sant[D]ificame
+t[A]oda mi v[D]ida
+P[D7]urif[G]icame, sant[D]ificame
+Esp[A]íritu Santo v[D]en, ven
+
+`
+  },
+    {
+    titulo: "Ven, Espíritu de Dios",
+    categoria: "otros",
+    tiempo: "Ordinario",
+    tono: "Dm",
+    autor: "Kerigma",
+    notas: "Canto del Espiritu Santo",
+    letra:`
+[coro]
+V[Dm]en, Espiritu de Dios.
+v[C]en, envuélveme en tu amor,
+V[Bb]en, Espíritu de Dios,
+ven a m[A7]i (2)
+
+[verso 1]
+Ú[Dm]ngeme, env[D7]uélveme c[Gm]on tu luz,
+Ú[Dm]ngeme, env[D7]uélveme c[F]on tu poder,
+Ú[Dm]ngeme, envu[D7]élveme con tu pres[Gm]encia,
+Ú[A7]ngeme, envuélveme con tu [Dm]amor,
+
+[verso 2]
+Libérame, purifícame con tu luz
+Libérame, purifícame con tu poder
+Libérame, purifícame con tu presencia
+Libérame, purifícame con tu amor
+
+[verso 3]
+Inúndame, condúceme con tu luz
+Inúndame, condúceme con tu poder
+Inúndame, condúceme con tu presencia
+Inúndame, condúceme con tu amor
+
+
+
+`
+  }
+];
