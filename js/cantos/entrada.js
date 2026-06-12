4ec5f9b1e14012686ae9dfccc950e80920085e75
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-12 17:38:23 -0600

    Update entrada.js

--- js/cantos/entrada.js
+++ js/cantos/entrada.js
@@ -106,13 +106,13 @@ en un gr[D]an lazo de am[E]or
 
 [verso 1]
 Porque Cr[D]isto ha demostr[E]ado
-con sus [A]obras l[C#7]a verd[F#m]ad
+con sus [A]obras l[C#m]a verd[F#m]ad
 porque s[D]olo un buen ap[E]ostol
 sentir[A]a felicid[A7]ad
 
 [verso 2]
 Porque el m[D]undo neces[E]ita
-mas am[A]or y l[C#7]ibert[F#m]ad
+mas am[A]or y l[C#m]ibert[F#m]ad
 es por [D]eso que ven[E]imos
 a cant[D]arl[E]e a D[A]ios
 `
