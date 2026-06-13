cd9b1bb94f30ea1b08076b78cacb6910e9b099d5
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-13 08:21:19 -0600

    Actualizar santo.js

--- js/cantos/santo.js
+++ js/cantos/santo.js
@@ -15,18 +15,19 @@ ll[C#7]enos est[F#m]án el c[B7]ielo y la t[E]ierra
 d[A]e tu gl[F#]moria hos[G#7]ann[C#m]a 
 
 H[C#7]osann[F#m]a, h[B7]osann[E]a, h[A]osann[F#m]a 
-[G#7]en el ciel[C#m]o 
+[G#7]en el c[C#m]ielo 
 H[C#7]osann[F#m]a, h[B7]osann[E]a, h[A]osann[F#m]a 
-[G#7]en el ciel[C#m]o  
+[G#7]en el c[C#m]ielo  
 
 B[C#7]endito el que v[F#m]iene 
 en n[B7]ombre del Señ[E]or 
 hos[A]anna en el c[F#m]ielo 
 hos[G#7]ann[C#m]a. 
 
-Hosanna, hosanna, hosanna 
-en el cielo 
-hosanna, hosanna, hosanna 
+H[C#7]osann[F#m]a, h[B7]osann[E]a, h[A]osann[F#m]a 
+[G#7]en el c[C#m]ielo 
+H[C#7]osann[F#m]a, h[B7]osann[E]a, h[A]osann[F#m]a 
+[G#7]en el c[C#m]ielo   
 en el cielo. 
 
 `
