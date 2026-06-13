68a4fc2991080a61b61bcacb5518968f1f4a8465
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-13 07:47:01 -0600

    Actualizar gloria.js

--- js/cantos/gloria.js
+++ js/cantos/gloria.js
@@ -35,15 +35,19 @@ P[C]adre, Padre, P[Am]adre
 Señor Dios h[Em]ijo
 pied[B7]ad, pied[Em]ad,
 pied[B7]ad Señ[Em]or
-Tu que qu[G]itas el pec[C]ado del m[Em]undo
+Tu que qu[G]itas
+el pec[C]ado del m[Em]undo
 esc[B7]uchan[Em]os, esc[B7]uchan[Em]os,
-tu que est[G]as a la der[C]echa del P[Em]adre
-pied[B7]ad, pied[Em]ad, pied[B7]ad Señ[Em]or.
+tu que est[G]as
+a la der[C]echa del P[Em]adre
+pied[B7]ad, pied[Em]ad,
+pied[B7]ad Señ[Em]or.
 
 [verso 3]
 Solo tu eres santo,
 s[B7]olo tu Señ[Em]or,
-s[G]olo tu altis[C]imo J[B7]esucrist[Em]o
+s[G]olo tu altis[C]imo
+J[B7]esucrist[Em]o
 con el Santo Espiritu
 en la gl[B7]oria de Dios P[Em]adre
 [G]amen, [C]amen, [B7]am[Em]en.
