4b1888acafac43d82e1cddf29611b0791ff40f2a
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-13 08:14:56 -0600

    Actualizar santo.js

--- js/cantos/santo.js
+++ js/cantos/santo.js
@@ -7,21 +7,22 @@ window.CANTOS_SANTO = [
     autor: "Kerigma",
     notas: "Canto de Santo",
     letra:`
+[C#m] [C#m]
 
-Santo, santo, santo
-es el Señor Dios del universo
-llenos están el cielo y la tierra 
-de tu gloria hosanna 
+S[C#7]ant[F#m]o, s[B7]mant[E]o, s[A]anto
+es el Señ[F#m]or Dios del [G#7]univ[C#m]erso
+ll[C#7]enos est[F#m]án el c[B7]ielo y la t[E]ierra 
+d[A]e tu gl[F#]moria hos[G#7]ann[C#m]a 
 
-Hosanna, hosanna, hosanna 
-en el cielo 
-hosanna, hosanna, hosanna 
-en el cielo. 
+H[C#7]osann[F#m]a, h[B7]osann[E]a, h[A]osann[F#m]a 
+[G#7]en el ciel[C#m]o 
+H[C#7]osann[F#m]a, h[B7]osann[E]a, h[A]osann[F#m]a 
+[G#7]en el ciel[C#m]o  
 
-Bendito el que viene 
-en nombre del Señor 
-hosanna en el cielo 
-hosanna. 
+B[C#7]endito el que v[F#m]iene 
+en n[B7]ombre del Señ[E]or 
+hos[A]anna en el c[F#m]ielo 
+hos[G#7]ann[C#m]a. 
 
 Hosanna, hosanna, hosanna 
 en el cielo 
