007b028afe83db10e4e864d2d6800eaa0d3a80d5
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-19 14:01:52 -0600

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -260,34 +260,29 @@ function alternarAcordes(){
 
 function mostrarPantalla(pantalla){
 
-document.getElementById("btnCantos")
-  .classList.remove("menu-activo");
-
-document.getElementById("btnFavoritosMenu")
-  .classList.remove("menu-activo");
-
-document.getElementById("btnMisa")
-  .classList.remove("menu-activo");
-
+  document.getElementById("btnCantos").classList.remove("menu-activo");
+  document.getElementById("btnFavoritosMenu").classList.remove("menu-activo");
+  document.getElementById("btnMisa").classList.remove("menu-activo");
 
   document.getElementById("pantallaCantos").style.display = "none";
   document.getElementById("pantallaFavoritos").style.display = "none";
   document.getElementById("pantallaMisa").style.display = "none";
 
   if(pantalla === "cantos"){
-  document.getElementById("pantallaCantos").style.display = "block";
-  document.getElementById("btnCantos").classList.add("menu-activo");
-}
+    document.getElementById("pantallaCantos").style.display = "block";
+    document.getElementById("btnCantos").classList.add("menu-activo");
+  }
 
   if(pantalla === "favoritos"){
-  document.getElementById("pantallaFavoritos").style.display = "block";
-  document.getElementById("btnFavoritosMenu").classList.add("menu-activo");
-  cargarFavoritosPantalla();
-}
+    document.getElementById("pantallaFavoritos").style.display = "block";
+    document.getElementById("btnFavoritosMenu").classList.add("menu-activo");
+    cargarFavoritosPantalla();
+  }
 
   if(pantalla === "misa"){
-  document.getElementById("pantallaMisa").style.display = "block";
-  document.getElementById("btnMisa").classList.add("menu-activo");
+    document.getElementById("pantallaMisa").style.display = "block";
+    document.getElementById("btnMisa").classList.add("menu-activo");
+  }
 }
 
 function cargarFavoritosPantalla(){
@@ -316,4 +311,3 @@ function cargarFavoritosPantalla(){
 }
 
 mostrarPantalla("cantos");
-
