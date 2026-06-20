52d31d296758fa5237a853b0af3175adb4c37b49
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-20 09:34:15 -0600

    Actualizar aleluya.js

--- js/cantos/aleluya.js
+++ js/cantos/aleluya.js
@@ -1,4 +1,25 @@
 window.CANTOS_ALELUYA = [
+{
+  titulo: "Aleluya Busca Primero",
+  categoria: "Aleluya",
+  tiempo: "Ordinario",
+  tono: "D",
+  autor: "Kerigma",
+  notas: "Canto del Aleluya",
+  letra:`
+[D]A Aleluy[A]a Ale [Bm]lu alelu[F#m]ya
+[G]A Alel[D]u alel[A]uya
+[D]A Aleluy[A]a Ale [Bm]lu alelu[F#m]ya
+[G]A Alel[D]uya alel[A]uy[D]a
+
+[Antifona antes del evangelio]
+
+[D]A Aleluy[A]a Ale l[Bm]u aleluy[F#m]a
+[G]A Alel[D]u alel[A]uy[D]a
+
+`
+},
+
   {
     titulo: "Aleluya miserere",
     categoria: "Aleluya",
