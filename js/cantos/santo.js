dc087ebda4b7f0283dc085a78c332a5dc7ac564e
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-20 16:52:07 -0600

    Actualizar santo.js

--- js/cantos/santo.js
+++ js/cantos/santo.js
@@ -7,11 +7,11 @@ window.CANTOS_SANTO = [
     autor: "Kerigma",
     notas: "Canto de Santo",
     letra:`
-[C#m]. [C#m]
+[C#m], [C#m]
 
 [verso1]
 
-S[C#7]ant[F#m]o, s[B7]ant[E]o, s[A]anto
+[C#7]San[F#m]to, [B7]sant[E]o, s[A]anto
 es el Señ[F#m]or Dios del [G#7]univ[C#m]erso
 ll[C#7]enos est[F#m]án el c[B7]ielo y la t[E]ierra 
 d[A]e tu gl[F#]oria hos[G#7]ann[C#m]a 
