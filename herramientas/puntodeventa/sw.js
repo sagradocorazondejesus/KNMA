6de0d511a07e57ba5389273d08202a713ecdb503
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-06-24 20:36:05 -0600

    Actualizar sw.js

--- herramientas/puntodeventa/sw.js
+++ herramientas/puntodeventa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pos-cache-v4";
+const CACHE_NAME = "pos-cache-v5";
 
 const FILES_TO_CACHE = [
   "./",
@@ -11,6 +11,8 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -18,10 +20,30 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const copia = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copia);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
