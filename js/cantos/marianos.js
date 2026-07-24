6c46edae2cde8982583e9d869c6875a1ad201224
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-07-24 15:00:02 -0600

    Actualizar marianos.js

--- js/cantos/marianos.js
+++ js/cantos/marianos.js
@@ -1,4 +1,43 @@
 window.CANTOS_MARIANOS = [
+{
+  titulo: "Mientras recorres la vida.",
+  categoria: "Salida",
+  tiempo: "Ordinario",
+  tono: "C",
+  autor: "Kerigma",
+  notas: "Salida, canto mariano",
+  letra:`
+[Verso 1]
+Mientras recorres la vida
+Tu nunca solo estás
+Contigo por el camino
+Santa María va
+
+[Coro]
+Ven con nosotros a caminar
+Santa María ven
+Ven con nosotros a caminar
+Santa María ven
+
+[Veros 2]
+Aunque te digan alguno
+Que nada puedes cambiar
+Lucha por un mundo nuevo
+Lucha por la verdad
+
+[Verso 3]
+Si por el mundo los hombres
+Sin conocerse van
+No niegues nunca tu mano
+Al que contigo está
+
+[Verso 4]
+Aunque parezcan tus pasos
+Inútil caminar
+Tu vas haciendo camino
+Otros lo seguiran
+`
+},
   {
     titulo: "Junto a ti María",
     categoria: "Marianos",
