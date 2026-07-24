93f14fdee723ee487f60289f97837798d3e3d608
Nuestra Señora de Guadalupe <kerigmanuevomilagrodeamor@gmail.com>
2026-07-24 14:40:44 -0600

    Actualizar ofertorio.js

--- js/cantos/ofertorio.js
+++ js/cantos/ofertorio.js
@@ -1,4 +1,50 @@
 window.CANTOS_OFERTORIO = [
+{
+  titulo: "Esto que te doy",
+  categoria: "Ofertorio",
+  tiempo: "Ordinario",
+  tono: "D",
+  autor: "Kerigma",
+  notas: "Canto de ofertorio, ordinario",
+  letra:`
+[Verso 1]
+[D]Esto que te d[F#m]oy
+Es v[G]ino y pan Señ[A7]or [G]
+Esto que te d[D]oy
+Es mi trab[A7]ajo [G]
+Es mi coraz[A7]ón mi alma [F#m]
+Es mi cuerpo y m[Bm]i razón [G]
+El esfuerzo d[A7]e mi camin[D]ar
+
+[Verso 2]
+Esto que te d[F#m]oy
+Mi v[G]ida es Señ[A7]or. [G]
+Es mi amor, tamb[D]ién es mi dol[A7]or [G]
+Es la imuci[A7]on, mis sueños [F#m]
+Es mi gozo y m[Bm]i llorar [G]
+Es mi canto [A7]y mi orac[D]ión
+
+[Coro]
+T[D]oma mi v[Fm]ida [G]
+Ponla en tu c[A7]orazón
+D[D]ame tu m[F#m]ano[G] y llévam[A7]e. [G]
+Cambia mi p[A7]an en tu carne [F#m]
+Y mi vino [Bm]en tu sangre
+Y a m[G]í Señor[A7] renuévam[D]e [B7]
+L[Em]impiame[A7] y sálvam[D]e
+
+[Verso 3]
+Esto que te d[F#m]oy
+No s[G]olo yo Señ[A7]or [G]
+Está voz tambi[D]én es de mi herm[A7]ano [G]
+Es la unión, la p[A7]az un orden [F#m]
+Armonía y fel[Bm]icidad [G]
+Es un canto [A7]en comunid[D]ad.
+
+
+
+`
+},
   {
     titulo: "Hoy Señor",
     categoria: "Ofertorio",
